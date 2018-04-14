c9859083b9084559c5933090d77950dc313e582a
Tomas <tcp.north@gmail.com>
2018-04-14 17:10:38 +0100

    Fixed Bug
    Bot not starting up

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.user.setGame(TEST BOT - DO NOT COMPILE)
-
 client.on('ready', () => {
     console.log('Bot is ready!');
 });
