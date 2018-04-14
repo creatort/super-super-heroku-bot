b7eda1886a013ee8c1df02644630d29a15951a0d
Tomas <tcp.north@gmail.com>
2018-04-14 17:01:13 +0100

    Updated the command list

--- bot.js
+++ bot.js
@@ -2,14 +2,28 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Bot is ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '#ping') {
+    	message.reply('Pong!');
   	}
 });
-
+client.on('message', message => {
+    if (message.content === '#pong') {
+    	message.reply('Ping! Hey waiiit!!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === '#help') {
+    	message.reply('**Commands:** ping, pong, help');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'whats tomasbots prefix?') {
+    	message.reply('#');
+  	}
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
