7447d4d339c4bf0e0bb5e677b3495d1e32c42362
Tomas <tcp.north@gmail.com>
2018-04-15 10:34:09 +0100

    Added 'clear' command
    May be deleted in future due to it not working

--- bot.js
+++ bot.js
@@ -36,5 +36,23 @@ client.on('message', message => {
         message.reply('Well, Here we go, I roll a ' + yesOrNo);
     }
 });
+
+let user = message.mentions.users.first();
+let amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+var prefix = '!'
+
+
+if (message.content.startsWith(prefix + 'clear') && !amount) 
+    return message.reply('Must specify an amount to clear!');
+if (message.content.startsWith(prefix + 'clear') && !amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to clear!');
+    message.channel.fetchMessages({
+        limit: amount,
+    }).then((messages) => {
+    if (user) {
+        const filterBy = user ? user.id : bot.user.id;
+        messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+    }
+    message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
+});
         // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
