a2d7b9bd895ce67a95770c0f55de06994f023ec4
Tomas <tcp.north@gmail.com>
2018-04-15 10:23:00 +0100

    Testing 8ball

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var rand = ['Yes', 'No', 'Why are you even trying?', 'What do you think? NO', 'Maybe', 'Never', 'Yep'];
+
+var yesOrNo = rand[Math.floor(Math.random()*rand.length)];
+
+
 client.on('ready', () => {
     console.log('Bot is ready to begin');
 });
@@ -26,5 +31,8 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '£8ball') {
+        message.reply('Well, Here we go, I roll a ' + yesOrNo')
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
