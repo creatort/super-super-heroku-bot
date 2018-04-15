37ff0498484088a4e63d492e70b7c46c71975700
Tomas <tcp.north@gmail.com>
2018-04-15 10:10:48 +0100

    Updated messages and prefix and deleted embed

--- bot.js
+++ bot.js
@@ -2,57 +2,29 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Bot is ready!');
+    console.log('Bot is ready to begin');
 });
 
 client.on('message', message => {
-    if (message.content === '{ping') {
+    if (message.content === '£ping') {
     	message.reply('Pong!');
   	}
 });
 client.on('message', message => {
-    if (message.content === '{pong') {
+    if (message.content === '£pong') {
     	message.reply('Ping! Hey waiiit!!');
   	}
 });
 client.on('message', message => {
-    if (message.content === '{help') {
+    if (message.content === '£help') {
     	message.reply('**Commands:** ping, pong, help');
   	}
 });
 client.on('message', message => {
     if (message.content === 'whats tomasbots prefix?') {
-    	message.reply('Currently it is {');
+    	message.reply('Currently it is "£"');
   	}
 });
-message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
-  }
-});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
