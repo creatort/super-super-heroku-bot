2f6a579a5b82e39bdfce7881ad8ffb6025e2b9d5
Tomas <tcp.north@gmail.com>
2018-04-15 10:37:34 +0100

    Removed Clear Command and added others
    - Removed Clear
    -Added 8ball Help
    - Edited 8ball message

--- bot.js
+++ bot.js
@@ -26,6 +26,11 @@ client.on('message', message => {
   	}
 });
 client.on('message', message => {
+    if (message.content === '£help 8ball') {
+    	message.reply('**8ball help** To get 8BALL to work you must use it as this; Ask the question in one message then use the 8ball command in the next.');
+  	}
+});
+client.on('message', message => {
     if (message.content === 'whats tomasbots prefix?') {
     	message.reply('Currently it is "£"');
   	}
@@ -33,26 +38,9 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '£8ball') {
-        message.reply('Well, Here we go, I roll a ' + yesOrNo);
+        message.reply('**' + yesOrNo '**');
     }
 });
 
-let user = message.mentions.users.first();
-let amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-var prefix = '!'
-
-
-if (message.content.startsWith(prefix + 'clear') && !amount) 
-    return message.reply('Must specify an amount to clear!');
-if (message.content.startsWith(prefix + 'clear') && !amount && !user) return message.reply('Must specify a user and amount, or just an amount, of messages to clear!');
-    message.channel.fetchMessages({
-        limit: amount,
-    }).then((messages) => {
-    if (user) {
-        const filterBy = user ? user.id : bot.user.id;
-        messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-    }
-    message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-});
         // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
